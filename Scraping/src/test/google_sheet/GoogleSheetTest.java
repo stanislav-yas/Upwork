78653f859a4ac8ea498862b5903094f411a79583
Stas <stanislav.yas@gmail.com>
2018-12-06 23:21:44 +0600

    06.12.18

--- Scraping/src/test/google_sheet/GoogleSheetTest.java
+++ Scraping/src/test/google_sheet/GoogleSheetTest.java
@@ -5,6 +5,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
@@ -33,7 +34,7 @@ public class GoogleSheetTest {
    * If modifying these scopes, delete your previously saved tokens/ folder.
    */
   private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
-  private static final String CREDENTIALS_FILE_PATH = "resources/credentials.json";
+  private static final String CREDENTIALS_FILE_PATH = "results/credentials.json";
 
   private static Credential getCredential() throws IOException, GeneralSecurityException {
     GoogleCredential credential = GoogleCredential.fromStream(new FileInputStream(CREDENTIALS_FILE_PATH))
@@ -55,9 +56,11 @@ public class GoogleSheetTest {
   @Test
   public void testSheet(){
     Credential credential = null;
+    NetHttpTransport transport = null;
     try{
       credential = getCredential();
-      Sheets service = new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, credential)
+      transport = GoogleNetHttpTransport.newTrustedTransport();
+      Sheets service = new Sheets.Builder(transport, JSON_FACTORY, credential)
           .setApplicationName(APPLICATION_NAME)
           .build();
       String sheetTitle = "My_testSheet";
@@ -68,11 +71,11 @@ public class GoogleSheetTest {
           .setFields("spreadsheetId")
           .execute();
       System.out.println("Spreadsheet ID: " + spreadsheet.getSpreadsheetId()); //Spreadsheet ID: 1ZptU7RYb9QZY_Ym1dx2GGbsLtCFsltqyir1QCJjz3Fg
+      //1M2-5JRUkuRL15r_04GPT4a4QKfEtX0SJyMkrk07RUm0
     }catch (Exception e ){
       System.out.println("Error occurred: " + e.getMessage());
       System.exit(1);
     }
-
 
 /*    // connect to google service with credential
     GoogleService googleService = new SpreadsheetService(APPLICATION_NAME);
