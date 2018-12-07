d4d8f2ba675d8cf52f6e90b869484a705630eff7
Stas <stanislav.yas@gmail.com>
2018-12-07 14:19:47 +0600

    readingSheetTest added

--- Scraping/src/test/google_sheet/GoogleSheetTest.java
+++ Scraping/src/test/google_sheet/GoogleSheetTest.java
@@ -1,24 +1,20 @@
 package google_sheet;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.Spreadsheet;
 import com.google.api.services.sheets.v4.model.SpreadsheetProperties;
+import com.google.api.services.sheets.v4.model.ValueRange;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
@@ -28,15 +24,18 @@ public class GoogleSheetTest {
   private static final String APPLICATION_NAME = "Google Sheets API Example";
   private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
   private static final String TOKENS_DIRECTORY_PATH = "tokens";
+  private static  Credential credential;
+  private static NetHttpTransport transport ;
+  private static final String SPREADSHEET_ID = "1KYwqJrJev8hvxNrpWYCd9hcYz_rB7WAmIIZsJVttZgE";
 
   /**
    * Global instance of the scopes required by this quickstart.
    * If modifying these scopes, delete your previously saved tokens/ folder.
    */
   private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
-  private static final String CREDENTIALS_FILE_PATH = "results/credentials.json";
+  private static final String CREDENTIALS_FILE_PATH = "resources/credentials.json";
 
-  private static Credential getCredential() throws IOException, GeneralSecurityException {
+  private static Credential getServiceCredential() throws IOException, GeneralSecurityException {
     GoogleCredential credential = GoogleCredential.fromStream(new FileInputStream(CREDENTIALS_FILE_PATH))
         .createScoped(SCOPES);
     return  credential;
@@ -53,27 +52,52 @@ public class GoogleSheetTest {
 
   }
 
-  @Test
-  public void testSheet(){
-    Credential credential = null;
-    NetHttpTransport transport = null;
-    try{
-      credential = getCredential();
+  private static NetHttpTransport getTransport() throws IOException, GeneralSecurityException{
+    if(transport == null){
       transport = GoogleNetHttpTransport.newTrustedTransport();
-      Sheets service = new Sheets.Builder(transport, JSON_FACTORY, credential)
+    }
+    return transport;
+  }
+
+  @Test
+  public void readingSheetTest(){
+    try{
+      if(credential == null){
+        credential = getServiceCredential();
+      }
+      Sheets sheetService = new Sheets.Builder(getTransport(), JSON_FACTORY, credential)
+          .setApplicationName(APPLICATION_NAME)
+          .build();
+      String range = "Sheet1!A1:D4";
+      ValueRange result = sheetService.spreadsheets().values().get(SPREADSHEET_ID, range).execute();
+      int numRows = result.getValues() != null ? result.getValues().size() : 0;
+      System.out.printf("%d rows retrieved.", numRows);
+    }catch (Exception e ) {
+      System.out.println("Reading Sheet error: " + e.getMessage());
+      System.exit(1);
+    }
+  }
+
+  @Test
+  public void createSheetTest(){
+    try{
+      if(credential == null){
+        credential = getServiceCredential();
+      }
+      Sheets sheetService = new Sheets.Builder(getTransport(), JSON_FACTORY, credential)
           .setApplicationName(APPLICATION_NAME)
           .build();
       String sheetTitle = "My_testSheet";
       Spreadsheet spreadsheet = new Spreadsheet()
           .setProperties(new SpreadsheetProperties()
               .setTitle(sheetTitle));
-      spreadsheet = service.spreadsheets().create(spreadsheet)
+      spreadsheet = sheetService.spreadsheets().create(spreadsheet)
           .setFields("spreadsheetId")
           .execute();
       System.out.println("Spreadsheet ID: " + spreadsheet.getSpreadsheetId()); //Spreadsheet ID: 1ZptU7RYb9QZY_Ym1dx2GGbsLtCFsltqyir1QCJjz3Fg
       //1M2-5JRUkuRL15r_04GPT4a4QKfEtX0SJyMkrk07RUm0
     }catch (Exception e ){
-      System.out.println("Error occurred: " + e.getMessage());
+      System.out.println("Creating Sheet error: " + e.getMessage());
       System.exit(1);
     }
 
